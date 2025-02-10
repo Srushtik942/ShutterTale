0be2683b1dc2f8a1651dea93364583161135d58a
srushtikulkarni <srushtikulkarni09@gmail.com>
2025-02-10 16:02:52 +0530

    feat: Adding function for seraching photos by tags but unable to fetch the PhotoId by tagsExist

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -1,5 +1,7 @@
+const { where } = require('sequelize');
 const axiosInstance = require('../library/axios.lib');
 const UNSPLASH_ACCESS_KEY = process.env.UNSPLASH_ACCESS_KEY ;
+const { photo:photModel, tags: tagModel} = require('../models');
 
 // Making API Calls to Unsplash
 
@@ -36,8 +38,36 @@ const searchImages =  async(req,res)=>{
         res.status(500).json({error: 'Failed to fetch images'});
     }
 }
+// Searching Photos by Tags and Sorting by Date Saved
 
+const searchPhotosByTags = async(req,res)=>{
+    try{
+        const {tag , sortOrder = 'ASC'} = req.query;
+
+     // Tag Validation
+     if(!tag){
+        return res.status(400).json({message:"Tag is required for seraching query!"});
+     }
+
+     //tag exists in db or not
+     const tagExist = await tagModel.findOne({where:{name: tag}});
+
+     const photoIds = await tagExist.findByPk({photoId});
+
+     const photos = await photModel.findAll({
+        where :{
+            tags:{tag}
+        },
+        order :[['dateSaved']]
+     })
+    return res.status(200).json({message:"Photos retrived successfully!",photos})
+
+    }catch(error){
+        return res.status(500).json({message:"Internal Server Error!", error: error.message});
+    }
+}
 
 module.exports = {
     searchImages,
+    searchPhotosByTags
 }
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { createNewUser} = require('./controllers/userController');
 const{searchImages} = require('./controllers/imageController');
 const {savePhotos} = require('./controllers/userController');
 const {addTags} = require('./controllers/userController');
+const {searchPhotosByTags} = require('./controllers/imageController')
 const  app = express();
 app.use(express.json());
 
@@ -10,6 +11,8 @@ app.post("/api/users", createNewUser);
 app.get("/api/search/photos",searchImages);
 app.post('/api/photos',savePhotos);
 app.post('/api/:photoId/tags',addTags);
+app.get('/api/photos/tag',searchPhotosByTags);
+// app.get('/api/search-history',trackHistory);
 
 const PORT = 3000
 
