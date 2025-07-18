f13faa1ea7357f6099c5739a33b95e03e8da3c04
srushtikulkarni <srushtikulkarni09@gmail.com>
2025-07-18 11:36:09 +0530

    Adding Production in config

--- config/config.js
+++ config/config.js
@@ -8,6 +8,14 @@ module.exports ={
     port: process.env.DB_PORT,
     dialect: 'postgres',
   },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3000,
+    dialect: 'postgres',
+  },
   test:{
     username: process.env.DB_USER,
     password:process.env.DB_PASSWORD,
